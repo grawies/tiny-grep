756fa97b9b1bb9fe89a51be884160f4c6cf3e7d5
Sam Zackrisson <samuelz@kth.se>
2017-04-18 20:25:56 +0200

    Add pseudocode for RegExp grammar parser.

--- src/parser.cpp
+++ src/parser.cpp
@@ -7,7 +7,33 @@
 namespace tinygrep {
 
 resyntax::RegExp parse(std::string s) {
-  // TODO: for realz
+  // TODO: Implement the parsing function based on the pseudocode.
+
+  //  init UNION-list
+  //  init CONCAT-list
+  //  while charstream is nonempty:
+  //    c <- next char
+  //    if c == ( :
+  //      subexpr <- find subexpr
+  //      CONCAT.append: recurse(subexpr)
+  //    if c == . :
+  //      CONCAT.append: DOT
+  //    if c == literal :
+  //      CONCAT.append: LITERAL(c)
+  //    if c in {+,?,*} :
+  //      CONCAT.top.modifywith(c)
+  //    if c == | :
+  //      cct <- CONCAT.CONCATENATEALL
+  //      UNION.append: cct
+  //      init CONCAT-list anew
+  //    else
+  //      throw a_fit
+  //  cct <- CONCAT.CONCATENATEALL
+  //  UNION.append: cct
+  //  regexp <- UNION.UNITEALL
+  //  return regexp
+
+  // This is a temporary placeholder construction for verifying that testing is functional.
   resyntax::RegExp a = resyntax::createLiteral('a');
   resyntax::RegExp b = resyntax::createLiteral('b');
   resyntax::RegExp c = resyntax::createLiteral('c');
