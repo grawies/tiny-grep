0d40d0bb18e4f344ec504bba301128f88ade3ede
Sam Zackrisson <samuelz@kth.se>
2017-04-27 12:36:38 +0200

    Add test for epsilon-NFA creation and matching.

--- test/test_spec.cpp
+++ test/test_spec.cpp
@@ -4,6 +4,7 @@
 #include <cxxtest/TestSuite.h>
 
 #include "linkedqueue/Queue.h"
+#include "enfa.h"
 #include "parser.h"
 #include "resyntax/resyntax.h"
 
@@ -29,20 +30,28 @@ class MyTestSuite : public CxxTest::TestSuite {
 
     void test2() {
       std::string regexp = "(a|b)*c";
+      // Parse a string to a RegExp AST.
       tinygrep::resyntax::RegExp ast = tinygrep::parse(regexp);
       TS_ASSERT_EQUALS(ast.getType(), tinygrep::resyntax::RegExpEnum::kConcatenation);
       tinygrep::resyntax::RegExp a_or_b_star = ast.getR1();
       tinygrep::resyntax::RegExp c = ast.getR2();
       TS_ASSERT_EQUALS(a_or_b_star.getR1().getR2().getType(), tinygrep::resyntax::RegExpEnum::kLiteral);
+      // Create a graph representation.
+      std::string regexp_test_graph = tinygrep::resyntax::to_graph(ast);
+      std::ofstream os_regexp("build/test_regexp_graph.gv");
+      if (os_regexp.is_open()) {
+        os_regexp << regexp_test_graph;
+      }
+      // Generate EpsilonNFA from AST.
+      tinygrep::enfa::EpsilonNFA enfautomaton = tinygrep::enfa::EpsilonNFA(ast);
+      TS_ASSERT(enfautomaton.accepts("abbaaababc"));
+      TS_ASSERT(enfautomaton.accepts("abc"));
+      TS_ASSERT(enfautomaton.accepts("bac"));
+      TS_ASSERT(enfautomaton.accepts("c"));
+      TS_ASSERT(!enfautomaton.accepts("cc"));
+      TS_ASSERT(!enfautomaton.accepts("abbaabababab"));
+      TS_ASSERT(!enfautomaton.accepts("acc"));
+      TS_ASSERT(!enfautomaton.accepts(""));
     }
 
-    void test3() {
-      std::string regexp = "(a|b)*c";
-      tinygrep::resyntax::RegExp ast = tinygrep::parse(regexp);
-      std::string regexp_test_graph = tinygrep::resyntax::to_graph(ast);
-      std::ofstream os("build/test3_graph.gv");
-      if (os.is_open()) {
-        os << regexp_test_graph;
-      }
-    }
 };
