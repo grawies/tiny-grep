c4bf48bb55f61fd2ff862eebf41144164c3fa64d
Sam Zackrisson <samuelz@kth.se>
2017-04-29 15:36:00 +0200

    Minor functional change to placeholder app functionality.

--- src/application.cpp
+++ src/application.cpp
@@ -10,15 +10,16 @@ int main(int argc, char* argv[]) {
   if (argc != 2) {
     std::cout << "Error: Bad argument count.\n";
   }
+  std::cout << "regex: " << argv[1] << "\n";
   // Currently: assume only regex argument, then search in stdin.
   // parse regexp
   std::string regex = argv[1];
   unsigned int matches = 0;
   try {
-    tinygrep::enfa::EpsilonNFA enfa(regex);
+    tinygrep::enfa::EpsilonNFA enfa("("+regex+")*");
     // while there is stdin, search in stdin
     std::string search_line;
-    while (std::cin >> search_line) {
+    while (std::getline(std::cin, search_line)) {
       // if match is found, output the matching line
       if (enfa.accepts(search_line)) {
         std::cout << search_line << "\n";
