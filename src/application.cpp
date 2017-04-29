e84abb9654bf15bd47ad438a31efa2b10d0d7d84
Sam Zackrisson <samuelz@kth.se>
2017-04-29 14:46:51 +0200

    Add barebones search functionality to application.

--- src/application.cpp
+++ src/application.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 #include "enfa.h"
 #include "parser.h"
 #include "resyntax/resyntax.h"
 
 int main(int argc, char* argv[]) {
-  std::cout << "Argument count: " << argc << std::endl;
-
+  if (argc != 2) {
+    std::cout << "Error: Bad argument count.\n";
+  }
+  // Currently: assume only regex argument, then search in stdin.
+  // parse regexp
+  std::string regex = argv[1];
+  unsigned int matches = 0;
+  try {
+    tinygrep::enfa::EpsilonNFA enfa(regex);
+    // while there is stdin, search in stdin
+    std::string search_line;
+    while (std::cin >> search_line) {
+      // if match is found, output the matching line
+      if (enfa.accepts(search_line)) {
+        std::cout << search_line << "\n";
+        matches++;
+      }
+    }
+  } catch (const std::domain_error& e) {
+    std::cout << "Error: Unsupported regex. " << e.what() << "\n";
+  }
+  std::cout << "Matches: " << matches << std::endl;
   return 0;
 }
