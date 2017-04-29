cb16b3c8d7f62380804bd54ca6620634bd92df2b
Sam Zackrisson <samuelz@kth.se>
2017-04-29 14:06:40 +0200

    Add tinygrep application main stub.

---
+++ src/application.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "enfa.h"
+#include "parser.h"
+#include "resyntax/resyntax.h"
+
+int main(int argc, char* argv[]) {
+  std::cout << "Argument count: " << argc << std::endl;
+
+  return 0;
+}
